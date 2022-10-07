81f5a56bf1e6d4b01f6f5a0a83fee7b054d92cf9
Saurav Kumar <saurav.0kmr@gmail.com>
2022-10-07 21:00:26 +0530

    worked on header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAYIvMqkQcjTIEMlhghkSa8DZwZUbxEKw0",
+    authDomain: "clone-c263f.firebaseapp.com",
+    projectId: "clone-c263f",
+    storageBucket: "clone-c263f.appspot.com",
+    messagingSenderId: "264376699734",
+    appId: "1:264376699734:web:a3898f0f1b79b8d8d464f7",
+    measurementId: "G-V5GWJ3BX46"
+  };
